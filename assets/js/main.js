085c4f9fe776a0494c4bf1e71ee34927bb205e8f
ahmetbahadircol <ahmetbahadircol@gmail.com>
2024-02-20 17:58:32 +0300

    wip 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,6 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .direction-l, .direction-r',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
